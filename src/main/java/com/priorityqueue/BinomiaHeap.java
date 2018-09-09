a6f98362de01786570999eb70cdd0d28630a8c85
wushudeng <sdwu@ruc.edu.cn>
2018-09-09 10:22:53 +0800

    Binomial Tree and its printer using java swing

---
+++ src/main/java/com/priorityqueue/BinomiaHeap.java
@@ -0,0 +1,197 @@
+package com.priorityqueue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Wushudeng on 2018/9/8.
+ */
+public class BinomiaHeap<T extends Comparable<? super T>> {
+    public static class BinomialNode<T extends Comparable<? super T>> implements Comparable<BinomialNode<T>>{
+        public T value;
+        public BinomialNode<T> child, sibling;
+        public int degree;
+
+        public BinomialNode(T value) {
+            this.value = value;
+            this.child = null;
+            this.sibling = null;
+            this.degree = 0;
+        }
+
+        public int compareTo(BinomialNode<T> o) {
+            return 0;
+        }
+    }
+
+    public BinomialNode<T> head;
+
+    public BinomiaHeap(BinomialNode<T> head) {
+        this.head = head;
+    }
+
+    public BinomiaHeap() {
+        this.head = null;
+    }
+
+    public void insert(T value) {
+        if (head == null) {
+            this.head = new BinomialNode<>(value);
+            return;
+        }
+        BinomiaHeap<T> heap = new BinomiaHeap<>(new BinomialNode<>(value));
+        this.union(heap);
+    }
+
+    private BinomialNode<T> meld(BinomiaHeap<T> heap) {
+        BinomialNode<T> new_head=null, node=null, r1=this.head, r2=heap.head;
+        while (r1!=null && r2!=null) {
+            if (r1.degree < r2.degree) {
+                if (new_head == null) {
+                    new_head = r1;
+                    node=new_head;
+                } else {
+                    node.sibling = r1;
+                    node = node.sibling;
+                }
+                r1 = r1.sibling;
+            } else {
+                if (new_head == null) {
+                    new_head = r2;
+                    node = new_head;
+                } else {
+                    node.sibling = r2;
+                    node = node.sibling;
+                }
+                r2 = r2.sibling;
+            }
+        }
+        if (r1 == null) {
+            node.sibling=r2;
+        } else {
+            node.sibling = r1;
+        }
+
+        return new_head;
+    }
+    private BinomialNode<T> link(BinomialNode<T> node, BinomialNode<T> next) {
+        if (node.value.compareTo(next.value)<0) {
+            next.sibling = node.child;
+            node.child = next;
+            node.degree++;
+            return node;
+        } else {
+            node.sibling = next.child;
+            next.child = node;
+            next.degree++;
+            return next;
+        }
+    }
+
+    public void union(BinomiaHeap<T> heap) {
+
+        if (heap.head == null) {
+            return;
+        }
+        if (this.head == null) {
+            this.head = heap.head;
+            return;
+        }
+        BinomialNode<T> meld_head = meld(heap);
+        BinomialNode<T> node = meld_head, pre=null, next=node.sibling, next_next;
+        while (next != null) {
+            next = node.sibling;
+            if (node.degree == next.degree && (next.sibling == null || next.degree != next.sibling.degree)) {
+                next_next = next.sibling;
+                node = link(node, next);
+                if (pre == null) {
+                    meld_head = node;
+                    next = next_next;
+                    node.sibling = next;
+                } else {
+                    pre.sibling = node;
+                    next = next_next;
+                    node.sibling = next;
+                }
+            } else {
+                pre = node;
+                node = next;
+                next = next.sibling;
+            }
+        }
+        this.head = meld_head;
+    }
+
+    public T get_min() {
+        if (head == null) {
+            return null;
+        }
+        BinomialNode<T> node = head.sibling;
+        T min = head.value;
+        while (node != null) {
+            if (min.compareTo(node.value) > 0) {
+                min = node.value;
+            }
+            node = node.sibling;
+        }
+        return min;
+    }
+
+    public T extract_min() {
+        if (head == null) {
+            return null;
+        }
+        BinomialNode<T> node = head, min=head, min_pre=null, node_pre=null;
+        while (node != null) {
+            if (min.value.compareTo(node.value)>0) {
+                min_pre = node_pre;
+                min = node;
+            }
+            node_pre = node;
+            node = node.sibling;
+        }
+        if (min_pre == null) {
+            head = min.sibling;
+        } else {
+            min_pre.sibling = min.sibling;
+        }
+        BinomialNode<T> child_node = min.child;
+        List<BinomialNode<T>> nodes = new ArrayList<>();
+        while (child_node != null) {
+            nodes.add(child_node);
+            child_node = child_node.sibling;
+        }
+
+        BinomiaHeap<T> heap = new BinomiaHeap<>();
+        heap.head = nodes.size()>=1 ? nodes.get(nodes.size()-1) : null;
+        BinomialNode<T> node1 = heap.head;
+        for (int i=nodes.size()-2; i>=0; i--) {
+            nodes.get(i).sibling=null;
+            node1.sibling = nodes.get(i);
+            node1 = node1.sibling;
+        }
+
+
+        union(heap);
+        return min.value;
+    }
+
+    public static void main(String[] args) {
+        BinomiaHeap<Integer> heap = new BinomiaHeap<>();
+        Integer[] list = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,33
+        };
+        for (Integer item:list) {
+            heap.insert(item);
+        }
+//        System.out.println(heap.extract_min());
+
+
+        BinomialHeapPrinter printer = new BinomialHeapPrinter(heap);
+        BinomialNode<Integer> root = heap.head;
+        printer.setSize(1000, 1000);
+        printer.paintTree(50, 50, root);
+        printer.setVisible(true);
+
+    }
+}
---
+++ src/main/java/com/priorityqueue/BinomialHeapPrinter.java
@@ -0,0 +1,142 @@
+package com.priorityqueue;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.font.GlyphVector;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+import java.util.*;
+import java.util.List;
+
+import com.priorityqueue.BinomiaHeap.BinomialNode;
+/**
+ * Created by Wushudeng on 2018/9/8.
+ */
+public class BinomialHeapPrinter extends JFrame{
+    int height = 50;
+    int width = 50;
+    int point_radius = 3;
+    BinomiaHeap<Integer> heap;
+    List<Shape> shapes = new ArrayList<>();
+    private static class StringShapes {
+        String s;
+        int x;
+        int y;
+
+        public StringShapes(String s, int x, int y) {
+            this.s = s;
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    List<StringShapes> string_shapes = new ArrayList<>();
+
+
+    public BinomialHeapPrinter(BinomiaHeap<Integer> heap) throws HeadlessException {
+        this.heap = heap;
+        setSize(600, 1000);
+    }
+
+    /**
+     * The maximum width of tree whose degree is n is M_n = C(n/2, n).
+     * For example, M_2=C(1, 2)=2, M_3=C(1, 3)=3,
+     * @param degree
+     * @return return the maximum width of one binomial tree. (Minus 1 from the maximum number of nodes in certain level of tree)
+     */
+    public static int getWidthBydegree(int degree) {
+        if (degree == 0 || degree == 1) {
+            return 0;
+        }
+        int[] dp = new int[degree+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        int i = 3;
+
+        while (i <= degree) {
+            dp[i] = 2*dp[i-1];
+            i++;
+        }
+        return dp[degree]-1;
+    }
+
+//    public static int calculateCombinations(int n, int m) {
+//        int dp_n, dp_m=1, dp_n_m=1;
+//        int i = 1, product=1;
+//        while (i<=n) {
+//            product*=i;
+//            if (i==m) {
+//                dp_m = product;
+//            }
+//            if (i== n-m) {
+//                dp_n_m = product;
+//            }
+//            i++;
+//        }
+//        dp_n = product;
+//
+//        return dp_n/(dp_m*dp_n_m);
+//    }
+
+    public void paint(Graphics g) {
+        super.paint(g);  // fixes the immediate problem.
+        Graphics2D graphics2D = (Graphics2D) g;
+        for (Shape shape:shapes) {
+            graphics2D.draw(shape);
+        }
+        for (StringShapes string_shape:string_shapes) {
+            graphics2D.drawString(string_shape.s, string_shape.x, string_shape.y);
+        }
+    }
+
+    public void paintTree(int root_x, int root_y, BinomialNode<Integer> root) {
+        if (root == null) {
+            return;
+        }
+        Shape oval = new Ellipse2D.Float(root_x, root_y, 10, 10);
+        shapes.add(oval);
+        string_shapes.add(new StringShapes(root.value.toString(), root_x, root_y));
+        if (root.child != null) {
+            BinomialNode<Integer> child = root.child;
+            int child_y = root_y + height;
+            int child_x = root_x - ((root.degree == 1) ? 0 : (getWidthBydegree(root.degree-1)+1)*width);
+            Shape line = new Line2D.Float(root_x+5, root_y+5, child_x, child_y);
+            shapes.add(line);
+
+            paintTree(child_x, child_y, child);
+            BinomialNode<Integer> sibling_next, sibling_pre=child;
+            int sibling_next_x, sibling_pre_x=child_x;
+
+            while (sibling_pre.sibling != null) {
+                sibling_next = sibling_pre.sibling;
+                sibling_next_x = sibling_pre_x+(getWidthBydegree(sibling_next.degree)+1)*width;
+                Shape l = new Line2D.Float(root_x+5, root_y+5, sibling_next_x+5, child_y+5);
+                shapes.add(l);
+                paintTree(sibling_next_x, child_y, sibling_next);
+                sibling_pre = sibling_next;
+                sibling_pre_x = sibling_next_x;
+            }
+        }
+
+        BinomialNode<Integer> sibling = root.sibling;
+        int sibling_x = root_x,
+            sibling_y = root_y;
+
+        while (sibling != null) {
+            sibling_x = sibling_x + (getWidthBydegree(sibling.degree)+1)*width;
+            paintTree(sibling_x, sibling_y, sibling);
+            sibling = sibling.sibling;
+        }
+
+    }
+    public static void main(String[] args) {
+//        BinomialHeapPrinter binomialHeapPrinter = new BinomialHeapPrinter(null);
+//        binomialHeapPrinter.setSize(1000, 1000);
+//        binomialHeapPrinter.setVisible(true);
+        getWidthBydegree(2);
+    }
+
+
+}
